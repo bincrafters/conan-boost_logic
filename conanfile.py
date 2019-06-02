4be3e7df18d6ab083e59ebe3310902da803747e6
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:06 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,15 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostLogicConan(base.BoostBaseConan):
     name = "boost_logic"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_logic"
-    lib_short_names = ["logic"]
-    header_only_libs = ["logic"]
-    b2_requires = [
-        "boost_config",
-        "boost_core"
-    ]
+    version = "1.70.0"
